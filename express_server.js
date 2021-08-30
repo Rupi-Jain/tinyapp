090d1885cdbbff44c550197f9a38c1a0af147886
Rupi-Jain <rupi.jain@gmail.com>
2021-08-30 18:16:20 -0400

    Second route to LongURLs and shortUrl is added

--- express_server.js
+++ express_server.js
@@ -10,30 +10,14 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
-app.get("/", (req, res) => {
-  res.send("Hello!");
-});
-
 app.get("/urls", (req, res) => {
   const templateVars = {urls: urlDatabase};
   res.render("urls_index", templateVars);
-})
+});
 
-// app.get("/urls.json", (req, res) => {
-//   res.send(urlDatabase);
-// });
-
-// app.get("/set", (req, res) => {
-//   const a = 1;
-//   res.send(`a = ${a}`);
-//  });
- 
-//  app.get("/fetch", (req, res) => {
-//   res.send(`a = ${a}`);
-//  });
-
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
+app.get("/urls/:shortURL", (req, res) => {
+  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
+  res.render("urls_show", templateVars);
 });
 
 app.listen(PORT, () => {
