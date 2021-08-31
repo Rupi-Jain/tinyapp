a72effdecf46d28eec45f0d505ded8fdcafddfa7
Rupi-Jain <rupi.jain@gmail.com>
2021-08-31 13:24:45 -0400

    Post method and generateRandomId is implemented

--- express_server.js
+++ express_server.js
@@ -4,6 +4,12 @@ const app = express();
 const PORT = 8080; // default port 8080
 
 app.set("view engine", 'ejs');
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
+function generateRandomString() {
+  return Math.random().toString(36).substr(2, 6);
+}
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -15,10 +21,36 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
-app.get("/urls/:shortURL", (req, res) => {
-  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
-  res.render("urls_show", templateVars);
+app.get("/u/:shortURL", (req, res) => {
+  if (urlDatabase.hasOwnProperty(req.params.shortURL)) {
+    const longURL = urlDatabase[req.params.shortURL];
+    res.redirect(longURL);
+  } else {
+    res.send("Invalid Short URL");
+  }
 });
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+app.get("/urls/:shortURL", (req, res) => {
+  if (urlDatabase.hasOwnProperty(req.params.shortURL)) {
+    const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
+    res.render("urls_show", templateVars);
+  } else {
+    res.send("Invalid Short URL");
+  }
+
+});
+
+app.post("/urls", (req, res) => {
+  const shortURL = generateRandomString()
+  const longURL = req.body.longURL;
+  urlDatabase[shortURL] = longURL;
+  res.redirect("/urls/" );         
+});
+
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
